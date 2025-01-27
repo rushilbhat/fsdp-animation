14d4aab41f676fbcdbb55edba20b7ade1ba340a1
rushilbhat <bhatrushil@gmail.com>
2025-01-27 11:25:18 +0000

    Add React Router and updat dependencies

--- src/App.js
+++ src/App.js
@@ -1,11 +1,19 @@
 import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import ModelSplitAnimation from './ModelSplitAnimation';
+import ShardingAnimation from './ShardingAnimation';
+import GatherUpdate from './GatherUpdate';
+
 
 const App = () => {
   return (
-    <div>
-      <ModelSplitAnimation />
-    </div>
+    <Router basename={process.env.PUBLIC_URL}>
+      <Routes>
+        <Route path="/model-split" element={<ModelSplitAnimation />} />
+        <Route path="/sharding" element={<ShardingAnimation />} />
+        <Route path="/gather-update" element={<GatherUpdate />} />
+      </Routes>
+    </Router>
   );
 };
 
