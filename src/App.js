16ca2c4db53e93c16684ede349f0ce2949b6ffcd
rushilbhat <bhatrushil@gmail.com>
2025-01-27 11:28:49 +0000

    Switch to HashRouter

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import React from 'react';
-import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import { HashRouter as Router, Routes, Route } from 'react-router-dom';  // Changed this line
 import ModelSplitAnimation from './ModelSplitAnimation';
 import ShardingAnimation from './ShardingAnimation';
 import GatherUpdate from './GatherUpdate';
 
-
 const App = () => {
   return (
-    <Router basename={process.env.PUBLIC_URL}>
+    <Router>  {/* Removed basename since HashRouter handles it differently */}
       <Routes>
         <Route path="/model-split" element={<ModelSplitAnimation />} />
         <Route path="/sharding" element={<ShardingAnimation />} />
